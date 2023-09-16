634d31652145cc8451098a6ae8cc40164d743235
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-09-16 23:27:27 +0200

    Fixed error in Relation model

--- models/Relation.js
+++ models/Relation.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
         static associate(models) {
             Relation.belongsTo(models.EventUser, {
                 foreignKey: "eventUser",
-                as: "eventUser",
+                as: "eventoUsuario",
             });
         }
     }
