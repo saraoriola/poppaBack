feae253c9bbdae1504112389cab4b0ec3e1817e9
saraoriola <saraoriola@outlook.com>
2023-09-17 13:18:40 +0200

    Buggi

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use('/users', require('./routes/users'));
 
 app.use("/events", require("./routes/events"));
-app.use("/companies", require("./routes/companies"));
+app.use("/organizations", require("./routes/organizations"));
 
 app.listen(PORT, () => console.log(`Server created successfully ${PORT}`));
 
---
+++ routes/organizations.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const OrganizationController = require("../controllers/OrganizationController");
+
+router.post("/createOrganization", OrganizationController.createOrganization);
+
+module.exports = router;
