9e68531a87aef06bea7f7b800a6a509df77e5f05
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-09-17 19:12:02 +0200

    Fixed error in Relation model

--- migrations/20230916113525-create-user.js
+++ migrations/20230916113525-create-user.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await queryInterface.createTable("USERs", {
+        await queryInterface.createTable("Users", {
             id: {
                 allowNull: false,
                 autoIncrement: true,
@@ -44,6 +44,6 @@ module.exports = {
         });
     },
     async down(queryInterface, Sequelize) {
-        await queryInterface.dropTable("USERs");
+        await queryInterface.dropTable("Users");
     },
 };
