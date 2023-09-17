e11e9c35920f4669f261de8d233b1c3f5067076f
saraoriola <saraoriola@outlook.com>
2023-09-17 14:03:30 +0200

    Buggi 2.0

--- routes/organizations.js
+++ routes/organizations.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const OrganizationController = require("../controllers/OrganizationController");
 
 router.post("/createOrganization", OrganizationController.createOrganization);
+router.put("/updateOrganization/:id", OrganizationController.updateOrganization);
 
 module.exports = router;
