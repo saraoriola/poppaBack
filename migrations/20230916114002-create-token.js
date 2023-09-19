6bfc9c51554e3762c3a52268e300abd1e383bdeb
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-09-19 08:40:33 +0200

    Added nodemailer file

--- migrations/20230916114002-create-token.js
+++ migrations/20230916114002-create-token.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await queryInterface.createTable("tokens", {
+        await queryInterface.createTable("Tokens", {
             id: {
                 allowNull: false,
                 autoIncrement: true,
@@ -26,6 +26,6 @@ module.exports = {
         });
     },
     async down(queryInterface, Sequelize) {
-        await queryInterface.dropTable("tokens");
+        await queryInterface.dropTable("Tokens");
     },
 };
