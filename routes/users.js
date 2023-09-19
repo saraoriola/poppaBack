a0dd3e3f3548c8bc780ee3c973dcb855fbed8721
AdrianRgGit <adrianramirezgalera@gmail.com>
2023-09-19 13:57:01 +0200

    implementada la validación de registro de usuario

--- routes/users.js
+++ routes/users.js
@@ -6,19 +6,21 @@ const router = express.Router();
 
 router.get("/getall", UserController.getAll);
 router.get("/getbyid/:id", UserController.getById);
-router.get("/name/:name",UserController.getUserByName);
-router.get("/confirm/:emailToken",UserController.confirm);
-router.get("/recoverPassword/:email",UserController.recoverPassword);
+router.get("/name/:name", UserController.getUserByName);
+router.get("/confirm/:emailToken", UserController.confirm);
+router.get("/recoverPassword/:email", UserController.recoverPassword);
 
-router.post("/register", uploadUserImages.single("avatar"), UserController.create);
+router.post(
+  "/register",
+  uploadUserImages.single("avatar"),
+  UserController.register
+);
 router.post("/login", UserController.login);
 
-router.put("/id/:id",UserController.update)
-router.put("/resetPassword/:recoverToken",UserController.resetPassword);
+router.put("/id/:id", UserController.update);
+router.put("/resetPassword/:recoverToken", UserController.resetPassword);
 
 router.delete("/logout", authentication, UserController.logout);
-router.delete("/delete/:id", UserController.delete)
+router.delete("/delete/:id", UserController.delete);
 
 module.exports = router;
-
-
