36ef943a088b84d0da3fe426e907c4f0e64a346b
AdrianRgGit <adrianramirezgalera@gmail.com>
2023-09-20 08:57:25 +0200

    solucionado problema con migración facility adress -> address

--- controllers/FacilityController.js
+++ controllers/FacilityController.js
@@ -2,6 +2,18 @@ const { Facilities } = require("../models/index.js");
 
 // NOTE: Address -> adress
 const FacilityController = {
+  async getAllFacilities(req, res) {
+    try {
+      const getAllFacilities = await Facilities.findAll();
+      res.status(200).send(getAllFacilities);
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .send({ message: "Hay un problema con el servidor", error });
+    }
+  },
+
   async createFacility(req, res) {
     try {
       const facility = await Facilities.create(req.body);
--- routes/facilities.js
+++ routes/facilities.js
@@ -3,6 +3,8 @@ const FacilityController = require("../controllers/FacilityController");
 const router = express.Router();
 const { authentication } = require("../middleware/authentication");
 
+router.get("/getall", FacilityController.getAllFacilities);
+
 router.post("/create", authentication, FacilityController.createFacility);
 
 module.exports = router;
