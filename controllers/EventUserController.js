49a26f8d294625701e9105ab245899585091d17d
AdrianRgGit <adrianramirezgalera@gmail.com>
2023-09-20 13:44:48 +0200

    terminado el model de eventUserController

--- controllers/EventUserController.js
+++ controllers/EventUserController.js
@@ -2,6 +2,48 @@ const express = require("express");
 const { EventUser } = require("../models/index.js");
 
 const EventUserController = {
+  async getAllEventUsers(req, res) {
+    try {
+      const eventUsers = await EventUser.findAll();
+
+      res.status(200).send({
+        message:
+          "Lista de usuarios que asisten a todos los eventos obtenida exitosamente",
+        eventUsers,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({
+        message: "Hubo un problema con el servidor",
+      });
+    }
+  },
+
+  async getAllEventUsersByEvent(req, res) {
+    try {
+      const { id } = req.params;
+
+      const eventUsers = await EventUser.findAll({ where: { event_id: id } });
+
+      if (!eventUsers || eventUsers.length === 0) {
+        return res
+          .status(404)
+          .send({ message: "No se han encontrado resultados" });
+      } else {
+        res.status(200).send({
+          message:
+            "Lista de usuarios que asisten al evento obtenida exitosamente",
+          eventUsers,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({
+        message: "Hubo un problema con el servidor",
+      });
+    }
+  },
+
   //NOTE: Esto será automático con el QR supongo. De momento lo dejo así.
   //NOTE: Falta traerme el event_id. Se seleccionará manualmente supongo. De momento lo dejo para pasarlo por body a pelo.
   async createEventUser(req, res) {
