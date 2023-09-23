ab96d4dcb430f4cfe51d52e8d2bbe165d5d7cc8d
saraoriola <saraoriola@outlook.com>
2023-09-23 18:39:05 +0200

    Añadidas entradas y salidas con hora formateada en España y formato 00:00-24:00

--- controllers/DashboardController.js
+++ controllers/DashboardController.js
@@ -45,35 +45,39 @@ const DashboardController = {
 
   async getArrivalAndDepartureTimeForEvent(req, res) {
     const { id } = req.params;
-
+  
     if (typeof id === 'undefined' || isNaN(id)) {
       return res.status(400).json({ message: 'ID de evento no válido' });
     }
-
+  
     try {
       const users = await EventUser.findAll({
         where: { event_id: id },
         attributes: ['user_id', 'arriveTime', 'leaveTime'],
       });
-
+  
       if (!users || users.length === 0) {
         return res.status(404).json({ message: 'No se encontraron usuarios para este evento' });
       }
-
+  
+      const hourOptions = { hour: '2-digit', minute: '2-digit' };
+  
       const userTimes = {};
       users.forEach((user) => {
         userTimes[user.user_id] = {
-          arriveTime: user.arriveTime,
-          leaveTime: user.leaveTime,
+          arriveTime: user.arriveTime.toLocaleTimeString('es-ES', hourOptions),
+          leaveTime: user.leaveTime.toLocaleTimeString('es-ES', hourOptions),
         };
       });
-
+  
       return res.status(200).json(userTimes);
     } catch (error) {
       console.error(error);
       return res.status(500).json({ message: 'Error del servidor' });
     }
   },
+  
+  
 
 };
 
