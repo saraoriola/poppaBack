987dc6fecde10da8a2f9387eac25039741e59280
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-09-24 14:27:24 +0200

    Changing CORS config

--- index.js
+++ index.js
@@ -12,14 +12,15 @@ const docs = require("./docs/index");
 const PORT = process.env.PORT || 3001;
 
 // NOTE: middleware
-const whitelist = ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "https://eventum-front.vercel.app"];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "https://eventum-front.vercel.app"];
 const corsOptions = {
     origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error());
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = "The CORS policy for this site does not " + "allow access from the specified Origin.";
+            return callback(new Error(msg), false);
         }
+        return callback(null, true);
     },
 };
 
