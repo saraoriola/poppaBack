5b2078560b6c8248c8ea79fc43b79f883320b967
victorcodigos <victor.macedo.dedeus@gmail.com>
2023-09-24 23:56:54 +0200

    Changes

--- routes/locations.js
+++ routes/locations.js
@@ -5,15 +5,43 @@ const { authentication, isAdmin } = require("../middleware/authentication");
 
 //NOTE: const { uploadUserImages } = require("../middleware/multer");
 
-router.get("/getlocationsdesc",authentication,LocationController.getLocationsDesc);
-router.get("/getlocationsasc",authentication,LocationController.getLocationsAsc);
+router.get(
+  "/getlocationsdesc",
+  authentication,
+  LocationController.getLocationsDesc
+);
+router.get(
+  "/getlocationsasc",
+  authentication,
+  LocationController.getLocationsAsc
+);
 router.get("/getbyid/:id", authentication, LocationController.getById);
-router.get("/getbycapacity/:capacity",authentication,LocationController.getByCapacity);
 
-router.post("/create", isAdmin,authentication,LocationController.createLocation);
+router.get(
+  "/getbycapacity/:capacity",
+  authentication,
+  LocationController.getByCapacity
+);
 
-router.put("/update/:id",isAdmin,authentication,LocationController.updateLocation);
+router.post(
+  "/create",
+  isAdmin,
+  authentication,
+  LocationController.createLocation
+);
 
-router.delete("/delete/:id",isAdmin,authentication, LocationController.deleteLocation);
+router.put(
+  "/update/:id",
+  isAdmin,
+  authentication,
+  LocationController.updateLocation
+);
+
+router.delete(
+  "/delete/:id",
+  isAdmin,
+  authentication,
+  LocationController.deleteLocation
+);
 
 module.exports = router;
