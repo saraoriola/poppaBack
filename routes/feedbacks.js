0e0c7840b2119d95ffe6ea4ab3965bf45f027128
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-09-24 14:17:26 +0200

    Fix naming

--- routes/feedbacks.js
+++ routes/feedbacks.js
@@ -1,42 +1,23 @@
 const express = require("express");
-const FeedbackController = require("../controllers/FeedbackController");
+const FeedBacksController = require("../controllers/FeedBacksController");
 const { authentication, isAdmin } = require("../middleware/authentication");
 const router = express.Router();
 
-router.get("/getbyid/:id", authentication, FeedbackController.getFeedBackById);
-router.get("/getall", authentication, FeedbackController.getAllFeedBack);
-router.get("/highertolower", authentication, FeedbackController.getInDesc);
-router.get("/lowertohigher", authentication, FeedbackController.getInAsc);
+router.get("/getbyid/:id", authentication, FeedBacksController.getFeedBackById);
+router.get("/getall", authentication, FeedBacksController.getAllFeedBack);
+router.get("/highertolower", authentication, FeedBacksController.getInDesc);
+router.get("/lowertohigher", authentication, FeedBacksController.getInAsc);
 
-router.post("/create", authentication, FeedbackController.createFeedBack);
-router.post("/valoration", authentication, FeedbackController.createValoration);
+router.post("/create", authentication, FeedBacksController.createFeedBack);
+router.post("/valoration", authentication, FeedBacksController.createValoration);
 
 //NOTE: Hacer un endpoint para que el usuario pueda actualizar solo sus feedbacks.
-router.put("/update/:id", authentication, FeedbackController.updateFeedBack); //NOTE: Estos los veo innecesarios, un admin para que querría actualizar los endpoints de un usuario?
-router.put(
-  "/update/valoration/:id",
-  authentication,
-  FeedbackController.updateValoration
-);
+router.put("/update/:id", authentication, FeedBacksController.updateFeedBack); //NOTE: Estos los veo innecesarios, un admin para que querría actualizar los endpoints de un usuario?
+router.put("/update/valoration/:id", authentication, FeedBacksController.updateValoration);
 
 //NOTE: Hacer un endpoint para que el usuario pueda borrar solo sus feedbacks.
-router.delete(
-  "/delete/valoration/:id",
-  isAdmin,
-  authentication,
-  FeedbackController.deleteValoration
-);
-router.delete(
-  "/delete/:id",
-  isAdmin,
-  authentication,
-  FeedbackController.deleteFeedBackById
-);
-router.delete(
-  "/deleteall",
-  isAdmin,
-  authentication,
-  FeedbackController.deleteAllFeedbacks
-);
+router.delete("/delete/valoration/:id", isAdmin, authentication, FeedBacksController.deleteValoration);
+router.delete("/delete/:id", isAdmin, authentication, FeedBacksController.deleteFeedBackById);
+router.delete("/deleteall", isAdmin, authentication, FeedBacksController.deleteAllFeedbacks);
 
 module.exports = router;
