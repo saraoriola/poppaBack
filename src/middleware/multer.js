1bfa53f70dd5463fe0d81b3143700e8899c87fed
AdrianRgGit <adrianramirezgalera@gmail.com>
2023-09-26 12:26:40 +0200

    arreglado lo de multer, falta lo de cyber

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,22 +1,25 @@
 const Multer = require("multer");
 const mimetypes = ["image/png", "image/jpg", "image/jpeg", "image/gif"];
-
 const generateUploadImageMulter = (path) =>
-
-    Multer({
-        storage: Multer.diskStorage({
-            destination: (req, file, cb) => cb(null, path),
-            filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname),
-        }),
-        fileFilter: (req, file, cb) => {
-            if (mimetypes.includes(file.mimetype)) cb(null, true);
-            else cb(null, false);
-        },
-        limits: { fileSize: 2 * 1024 * 1024 },
-    });
-
-const uploadUserImages = generateUploadImageMulter("./assets/images/userImages");
-const uploadCommentImages = generateUploadImageMulter("./assets/images");
-const uploadPostImages = generateUploadImageMulter("./assets/images");
-
-module.exports = { uploadPostImages, uploadUserImages, uploadCommentImages };
+  Multer({
+    storage: Multer.diskStorage({
+      destination: (req, file, cb) => cb(null, path),
+      filename: (req, file, cb) =>
+        cb(null, Date.now() + "-" + file.originalname),
+    }),
+    fileFilter: (req, file, cb) => {
+      if (mimetypes.includes(file.mimetype)) cb(null, true);
+      else cb(null, false);
+    },
+    limits: { fileSize: 2 * 1024 * 1024 },
+  });
+const uploadUserImages = generateUploadImageMulter(
+  "src/assets/images/userImages"
+);
+const uploadFeedbackImages = generateUploadImageMulter(
+  "src/assets/images/feedbackImages"
+);
+const uploadEventImages = generateUploadImageMulter(
+  "src/assets/images/eventImages"
+);
+module.exports = { uploadEventImages, uploadUserImages, uploadFeedbackImages };
