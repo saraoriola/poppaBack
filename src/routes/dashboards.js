566845f3e6da7d9bf84688e735e5e58a978db4dc
saraoriola <saraoriola@outlook.com>
2023-09-26 09:36:34 +0200

    AñadidoDash controller

--- src/routes/dashboards.js
+++ src/routes/dashboards.js
@@ -4,13 +4,5 @@ const DashboardController = require("../controllers/DashboardController");
 const { authentication, isAdmin } = require("../middleware/authentication");
 
 router.get("/event/:id", DashboardController.getEventById);
-router.get("/event/:id/getCapacity", DashboardController.getCapacity);
-router.get("/event/:id/getSpeacker", DashboardController.getSpeacker);
-router.get("/event/:id/getTimes", DashboardController.getTimes);
-router.get('/event/:id/getType', DashboardController.getType);
-router.get('/event/:id/getAttendees', DashboardController.getAttendees);
-
-router.get("/user/:id", DashboardController.getUserById);
-router.get("/event/:id/users/getCountry", DashboardController.getCountry);
 
 module.exports = router;
